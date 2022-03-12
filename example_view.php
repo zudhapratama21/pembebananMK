913fc43d309dfeda5b5e47ddb4cba6c4231a242c
nadia indah <zudhapratama123@gmail.com>
2022-03-12 23:55:17 +0700

    ../

--- example_view.php
+++ example_view.php
@@ -17,19 +17,25 @@ STOK NUMBER(2)
 
 include "includes/func.inc.php";
 
-$db_user = "pa_example";
-$db_pass = "123";
+$db_user = "PA0010";
+$db_pass = "742309";
 $con = konekDb($db_user, $db_pass);
 
 echo "<strong>VIEW DATA BARANG</strong><br><br>";
-$sql = "SELECT nama_barang, stok FROM barang ORDER BY nama_barang";
-$hasil = query_view($con, $sql);
 
+$nomor = 111;
+$sql = "SELECT * FROM PEGAWAI WHERE NOMOR=:v1";
+
+$data = array(
+    ':v1' => $nomor
+);
+
+$hasil = query_view($con, $sql,$data);
 oci_fetch_all($hasil, $rows, 0, 0, OCI_FETCHSTATEMENT_BY_ROW);
 
     
 foreach ($rows as $hasil) {
-        echo "Nama Barang : " . $hasil['NAMA_BARANG'] ." - Stok : " . $hasil['STOK'] . "<br>";
+        echo "Nama Barang : " . $hasil['NIP'] ." - Stok : " . $hasil['NAMA'] . "<br>";
     }
 ?>
 </body>
